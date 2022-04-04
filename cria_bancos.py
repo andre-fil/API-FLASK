366ea759eb87061b0d80ce48e858901e9f2dfafc
André Filipe Sousa  Barreto <barreto.andre@discente.ufma.br>
2022-04-04 12:03:41 -0300

    função de inserir tabela no BD

--- cria_bancos.py
+++ cria_bancos.py
@@ -6,6 +6,9 @@ cursor = connection.cursor()
 cria_tabela = "CREATE TABLE IF NOT EXISTS Hoteis(Hotel_id text PRIMARY KEY,\
                Nome text,Estrelas real,Diaria real,Cidade text)"
 
+cria_hotel = "INSERT INTO Hoteis VALUES ('01','Hotellum',3.4,154.75,'São Luis')"
+
 cursor.execute(cria_tabela)
+cursor.execute(cria_hotel)
 connection.commit()
 connection.close()
